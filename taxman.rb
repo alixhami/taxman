a8d43fc0f451908d6d80b4a56b6e3c98df4b0260
Alix Hamilton <alix.hamilton@gmail.com>
2016-07-24 13:21:06 -0700

    reformatted from being one giant method

--- taxman.rb
+++ taxman.rb
@@ -17,87 +17,83 @@ end
 puts "How much is your yearly income? If you're married, include your spouse's income."
 gross_income = gets.chomp.to_i
 
-def calc_estimated_taxes(filing_status, gross_income)
-  case filing_status  # determine tax brackets
-  when "single"
-    personal_exemption = 4050
-    standard_deduction = 6300
-    pep_threshold = 258250
-    cap_10 = 9275.0
-    cap_15 = 37650.0
-    cap_25 = 91150.0
-    cap_28 = 190150.0
-    cap_33 = 413350.0
-    cap_35 = 415050.0
-  when "mfj"
-    personal_exemption = 8100
-    standard_deduction = 12600
-    pep_threshold = 309900
-    cap_10 = 18550.0
-    cap_15 = 75300.0
-    cap_25 = 151900.0
-    cap_28 = 231450.0
-    cap_33 = 413350.0
-    cap_35 = 466950.0
-  end
-
-  # personal exemption phaseout
-  # must reduce the dollar amout of exemptions by 2% for each $2500 or part of $2500 that AGI exceeds threshold
-  # If AGI exceeds the theshold by more than $122,500, the amount of your deduction for exemptions is reduced to zero.
-  if gross_income > pep_threshold
-    number_of_reductions = ((gross_income - pep_threshold)/2500.0).ceil
-    percentage_reduction = 0.02 * number_of_reductions
-    if gross_income - pep_threshold >= 122500
-      personal_exemption = 0
-    else
-      personal_exemption *= 1 - percentage_reduction
-    end
-  end
-
-  # calculate taxable income
-  taxable_income = gross_income - standard_deduction - personal_exemption
-
-  # these variables store the maximum tax at each rate
-  max_10 = cap_10 * 0.1
-  max_15 = ((cap_15 - cap_10) * 0.15) + max_10
-  max_25 = ((cap_25 - cap_15) * 0.25) + max_15
-  max_28 = ((cap_28 - cap_25) * 0.28) + max_25
-  max_33 = ((cap_33 - cap_28) * 0.33) + max_28
-  max_35 = ((cap_35 - cap_33) * 0.35) + max_33
-
-  # calculate estimated taxes and marginal rate
-  if taxable_income <= 0
-    estimated_taxes = 0
-    marginal_rate = "0%"
-    average_rate = "0"
-  elsif taxable_income <= cap_10
-    estimated_taxes = taxable_income * 0.10
-    marginal_rate = "10%"
-  elsif taxable_income <= cap_15
-    estimated_taxes = max_10 + ((taxable_income - cap_10) * 0.15)
-    marginal_rate = "15%"
-  elsif taxable_income <= cap_25
-    estimated_taxes = max_15 + ((taxable_income - cap_15) * 0.25)
-    marginal_rate = "25%"
-  elsif taxable_income <= cap_28
-    estimated_taxes = max_25 + ((taxable_income - cap_25) * 0.28)
-    marginal_rate = "28%"
-  elsif taxable_income <= cap_33
-    estimated_taxes = max_28 + ((taxable_income - cap_28) * 0.33)
-    marginal_rate = "33%"
-  elsif taxable_income <= cap_35
-    estimated_taxes = max_33 + ((taxable_income - cap_33) * 0.35)
-    marginal_rate = "35%"
-  else
-    estimated_taxes = max_35 + ((taxable_income - cap_35) * 0.396)
-    marginal_rate = "39.6%"
-  end
-
-  average_rate ||= (estimated_taxes/gross_income*100).to_i
-
-  puts "Your estimated 2016 income taxes are $#{estimated_taxes.to_i}"
-  puts "Your marginal tax rate is #{marginal_rate}"
-  puts "Your average tax rate is #{average_rate}%"
+case filing_status  # determine tax brackets
+when "single"
+  personal_exemption = 4050
+  standard_deduction = 6300
+  pep_threshold = 258250
+  cap_10 = 9275.0
+  cap_15 = 37650.0
+  cap_25 = 91150.0
+  cap_28 = 190150.0
+  cap_33 = 413350.0
+  cap_35 = 415050.0
+when "mfj"
+  personal_exemption = 8100
+  standard_deduction = 12600
+  pep_threshold = 309900
+  cap_10 = 18550.0
+  cap_15 = 75300.0
+  cap_25 = 151900.0
+  cap_28 = 231450.0
+  cap_33 = 413350.0
+  cap_35 = 466950.0
 end
 
-calc_estimated_taxes(filing_status, gross_income)
+# these variables store the maximum tax at each rate
+max_10 = cap_10 * 0.1
+max_15 = ((cap_15 - cap_10) * 0.15) + max_10
+max_25 = ((cap_25 - cap_15) * 0.25) + max_15
+max_28 = ((cap_28 - cap_25) * 0.28) + max_25
+max_33 = ((cap_33 - cap_28) * 0.33) + max_28
+max_35 = ((cap_35 - cap_33) * 0.35) + max_33
+
+# personal exemption phaseout
+# must reduce the dollar amout of exemptions by 2% for each $2500 or part of $2500 that AGI exceeds threshold
+# If AGI exceeds the theshold by more than $122,500, the amount of your deduction for exemptions is reduced to zero.
+if gross_income > pep_threshold
+  number_of_reductions = ((gross_income - pep_threshold)/2500.0).ceil
+  percentage_reduction = 0.02 * number_of_reductions
+  if gross_income - pep_threshold >= 122500
+    personal_exemption = 0
+  else
+    personal_exemption *= 1 - percentage_reduction
+  end
+end
+
+# calculate taxable income
+taxable_income = gross_income - standard_deduction - personal_exemption
+
+# calculate estimated taxes and marginal rate
+if taxable_income <= 0
+  estimated_taxes = 0
+  marginal_rate = "0%"
+  average_rate = "0"
+elsif taxable_income <= cap_10
+  estimated_taxes = taxable_income * 0.10
+  marginal_rate = "10%"
+elsif taxable_income <= cap_15
+  estimated_taxes = max_10 + ((taxable_income - cap_10) * 0.15)
+  marginal_rate = "15%"
+elsif taxable_income <= cap_25
+  estimated_taxes = max_15 + ((taxable_income - cap_15) * 0.25)
+  marginal_rate = "25%"
+elsif taxable_income <= cap_28
+  estimated_taxes = max_25 + ((taxable_income - cap_25) * 0.28)
+  marginal_rate = "28%"
+elsif taxable_income <= cap_33
+  estimated_taxes = max_28 + ((taxable_income - cap_28) * 0.33)
+  marginal_rate = "33%"
+elsif taxable_income <= cap_35
+  estimated_taxes = max_33 + ((taxable_income - cap_33) * 0.35)
+  marginal_rate = "35%"
+else
+  estimated_taxes = max_35 + ((taxable_income - cap_35) * 0.396)
+  marginal_rate = "39.6%"
+end
+
+average_rate ||= (estimated_taxes/gross_income*100).to_i
+
+puts "Your estimated 2016 income taxes are $#{estimated_taxes.to_i}"
+puts "Your marginal tax rate is #{marginal_rate}"
+puts "Your average tax rate is #{average_rate}%"
