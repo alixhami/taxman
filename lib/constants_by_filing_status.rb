dcdd2e199014966da946d4d788bd8adc330f7b85
Alix Hamilton <alix.hamilton@gmail.com>
2016-08-13 15:35:33 -0700

    added mfs and head of household constants

--- lib/constants_by_filing_status.rb
+++ lib/constants_by_filing_status.rb
@@ -17,22 +17,46 @@ module Tax
   	  	{rate: 0.33, limit: 413350.0},
   	  	{rate: 0.35, limit: 466950.0},
   	  	{rate: 0.396}
-  	  	]
+      ],
+      mfs: [
+        {rate: 0.10, limit: 9275.0},
+  	  	{rate: 0.15, limit: 37650.0},
+  	  	{rate: 0.25, limit: 75950.0},
+  	  	{rate: 0.28, limit: 115725.0},
+  	  	{rate: 0.33, limit: 206675.0},
+  	  	{rate: 0.35, limit: 233475.0},
+  	  	{rate: 0.396}
+      ],
+      head_of_household: [
+        {rate: 0.10, limit: 13250.0},
+  	  	{rate: 0.15, limit: 50400.0},
+  	  	{rate: 0.25, limit: 130150.0},
+  	  	{rate: 0.28, limit: 210800.0},
+  	  	{rate: 0.33, limit: 413350.0},
+  	  	{rate: 0.35, limit: 441000.0},
+  	  	{rate: 0.396}
+      ]
   	}
 
   STANDARD_DEDUCTION = {
     single: 6300,
-    mfj: 12600
+    mfj: 12600,
+    mfs: 6300,
+    head_of_household: 9300
   }
 
   PERSONAL_EXEMPTION = {
     single: 4050,
-    mfj: 8100
+    mfj: 8100,
+    mfs: 4050,
+    head_of_household: 4050
   }
 
   PEP_THRESHOLD = {
     single: 258250,
-    mfj: 309900
+    mfj: 309900,
+    mfs: 154950,
+    head_of_household: 284050
   }
 
 end
